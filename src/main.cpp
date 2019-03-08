7d59dd8d5e677c2739b34d06b502cddb719a39e7
Boban Joksimoski <boban.joksimoski@gmx.com>
2019-03-08 03:47:49 +0100

    Reformatting

--- src/main.cpp
+++ src/main.cpp
@@ -122,9 +122,8 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO,
-    // but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs)
-    // when it's not directly necessary.
+    // but this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways
+    // so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
 
