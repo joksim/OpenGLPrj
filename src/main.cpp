9d37a6e1f4162513de8d14cb06ae0167215797e4
Boban Joksimoski <boban.joksimoski@gmx.com>
2019-05-02 08:53:57 +0200

    coordinate-systems-3

--- src/main.cpp
+++ src/main.cpp
@@ -172,6 +172,8 @@ int main() {
   // or set it via the texture class
   ourShader.setInt("texture2", 1);
 
+  glEnable(GL_DEPTH_TEST);
+
   // render loop
   // -----------
   while (!glfwWindowShouldClose(window)) {
@@ -180,7 +182,7 @@ int main() {
     // render
     // ------
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // bind textures on corresponding texture units
     glActiveTexture(GL_TEXTURE0);
