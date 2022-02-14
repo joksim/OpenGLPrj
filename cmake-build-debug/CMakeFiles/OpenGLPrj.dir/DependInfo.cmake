63fc3f659f155382b1381fc7b894185548ab547a
FilipKostov <kostovfilip5@yahoo.com>
2022-02-14 12:25:20 +0100

    Posle dolgo vreme promena

---
+++ cmake-build-debug/CMakeFiles/OpenGLPrj.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Korisnik/Desktop/OpenGLPrj-1/vendor/glad/src/glad.c" "C:/Users/Korisnik/Desktop/OpenGLPrj-1/cmake-build-debug/CMakeFiles/OpenGLPrj.dir/vendor/glad/src/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"C:/Users/Korisnik/Desktop/OpenGLPrj-1\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../vendor/glad/include"
+  "../vendor/glfw/include"
+  "../vendor/glm"
+  "../vendor/stb"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Korisnik/Desktop/OpenGLPrj-1/src/Shader.cpp" "C:/Users/Korisnik/Desktop/OpenGLPrj-1/cmake-build-debug/CMakeFiles/OpenGLPrj.dir/src/Shader.cpp.obj"
+  "C:/Users/Korisnik/Desktop/OpenGLPrj-1/src/main.cpp" "C:/Users/Korisnik/Desktop/OpenGLPrj-1/cmake-build-debug/CMakeFiles/OpenGLPrj.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  "PROJECT_SOURCE_DIR=\"C:/Users/Korisnik/Desktop/OpenGLPrj-1\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../vendor/glad/include"
+  "../vendor/glfw/include"
+  "../vendor/glm"
+  "../vendor/stb"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Korisnik/Desktop/OpenGLPrj-1/cmake-build-debug/vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
